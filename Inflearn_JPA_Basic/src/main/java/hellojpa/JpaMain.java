ebf6505a7846a6ad38a10e72fa2174c4503e9237
공대표 <c5ngsdp@gmail.com>
2024-07-18 20:49:20 +0900

    Section 2. JPA 시작하기
    Hello JPA  - 애플리케이션 개발

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,41 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
-
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
-        emf.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            /* Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloB");
+
+            em.persist(member); */ // 등록
+
+            Member findMember = em.find(Member.class, 1L); // 조회
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
+
+            findMember.setName("HelloJPA"); // 수정
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList(); // JPQL
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }

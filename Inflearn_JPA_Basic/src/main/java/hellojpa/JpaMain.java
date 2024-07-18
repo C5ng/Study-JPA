e1259413563388643de78b08f14887b59a842e96
공대표 <c5ngsdp@gmail.com>
2024-07-18 21:18:32 +0900

    Section 2. 영속성 관리 - 내부 동작 방식
    영속성 컨텍스트 2

--- Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic/src/main/java/hellojpa/JpaMain.java
@@ -34,20 +34,36 @@ public class JpaMain {
             /* 영속성 컨텍스트 1 */
 
             // 비영속
-            Member member = new Member();
+            /* Member member = new Member();
             member.setId(100L);
             member.setName("HelloJPA");
 
             // 영속
-            em.persist(member);
+            em.persist(member); */
 
             // 준영속, 분리
-            em.detach(member);
+            /* em.detach(member);
 
             // 삭제
-            em.refresh(member);
+            em.refresh(member); */
 
             /* 영속성 컨텍스트 1 */
+
+            /* 영속성 컨텍스트 2 */
+
+            /* Member findMember1 = em.find(Member.class, 100L); // 조회를 해도 Select 쿼리가 실행되지 않는다. -> 1차 캐시에서 조회하기 때문
+
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2); */ // 쓰기 지연 저장소에 쌓아둔다.
+            System.out.println("=======================");
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("HI"); // 변경 감지
+
+            /* 영속성 컨텍스트 2 */
 
             tx.commit();
         } catch (Exception e) {

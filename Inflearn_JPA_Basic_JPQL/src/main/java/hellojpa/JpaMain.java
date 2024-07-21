dda83578117e51ed74d44b4937060d9048ebaf71
공대표 <c5ngsdp@gmail.com>
2024-07-21 15:31:23 +0900

    Section 10. 객체지향 쿼리 언어 1 - 기본 문법
    프로젝션(SELECT)

--- Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
+++ Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,11 @@
 package hellojpa;
 
 import hellojpa.jpql.Member;
+import hellojpa.jpql.MemberDTO;
 import jakarta.persistence.*;
+import org.h2.command.dml.Merge;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -19,13 +23,25 @@ public class JpaMain {
             member.setUsername("member1");
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class); // 타입 정보를 명확하게 적어서 TypeQuery
             Query query1 = em.createQuery("select m.username, m.age from Member m", String.class); // 반환 타입이 명확하지 않아 Query
 
             em.createQuery("select m from Member m where m.username = :username", Member.class)
                     .setParameter("username", "member1"); // 파라미터 이름 기준
-            em.createQuery("select m from Member m where m.username = :username", Member.class)
-                    .setParameter(1, "member1"); // 파라미터 이름 기준
+//            em.createQuery("select m from Member m where m.username = :username", Member.class)
+//                    .setParameter(1, "member1"); // 파라미터 이름 기준
+
+            List<Member> result = em.createQuery("select m from Member m", Member.class)
+                    .getResultList(); // 전부 영속성 컨텍스트에 관리된다.
+
+            List<MemberDTO> resultList = em.createQuery("select new hellojpa.jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList(); // new 명령어로 조회
+
+            System.out.println("resultList = " + resultList);
+
 
             tx.commit();
         } catch (Exception e) {
---
+++ Inflearn_JPA_Basic_JPQL/src/main/java/hellojpa/jpql/MemberDTO.java
@@ -0,0 +1,11 @@
+package hellojpa.jpql;
+
+public class MemberDTO {
+    private String username;
+    private int age;
+
+    public MemberDTO(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+}

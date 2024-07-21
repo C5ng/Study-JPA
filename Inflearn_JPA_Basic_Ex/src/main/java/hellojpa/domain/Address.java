a5b6163e688266eec52f57e00546e294b8c28cb6
공대표 <c5ngsdp@gmail.com>
2024-07-21 14:12:13 +0900

    Section 9. 값 타입
    실전 예제 6 - 값 타입 매핑

---
+++ Inflearn_JPA_Basic_Ex/src/main/java/hellojpa/domain/Address.java
@@ -0,0 +1,50 @@
+package hellojpa.domain;
+
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Address address = (Address) object;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    } // Proxy 객체는 필드에 직접 접근하는 것보다 get으로 호출하는 것이 더 안전하다.
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}

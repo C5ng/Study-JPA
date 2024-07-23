149599bfecc60dbac95878e94c3e53d86b2a6218
DESKTOP-UNJHOPU\user <c5ngsdp@gmail.com>
2024-07-23 14:03:09 +0900

    Section 2. 도메인 분석 설계
    엔티티 클래스 개발 2

--- Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/domain/Address.java
+++ Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,4 +9,12 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {} // JPA 스펙 상 기본 생성자를 선언해야 하는데 protected로 제한한다.
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    } // setter는 제공하지 않고 생성자로만 생성 가능하도록 설계
 }
---
+++ Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"), // 중간 테이블에 존재하는 category_id
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+    // 해당 엔티티 내에서 연관관계 매핑한다. 즉 자기를 매핑
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}

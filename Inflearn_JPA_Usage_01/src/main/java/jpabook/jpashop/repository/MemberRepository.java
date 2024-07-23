c0141f0533ec08be6ee2736fdca5e8fd14c7ec2b
DESKTOP-UNJHOPU\user <c5ngsdp@gmail.com>
2024-07-23 15:34:05 +0900

    Section 4. 회원 도메인 개발
    회원 서비스 개발

--- Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,14 +3,15 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em; // Spring이 EntityManage를 만들어 Injection 해준다.
+    private final EntityManager em; // Spring이 EntityManage를 만들어 Injection 해준다.
 
     public void save(Member member) {
         em.persist(member);

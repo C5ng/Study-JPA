00450c65e0057ec4a80df5ca464d212b13ee2381
DESKTOP-UNJHOPU\user <c5ngsdp@gmail.com>
2024-07-26 10:29:37 +0900

    Section 3. API 개발 고급 - 지연 로딩과 조회 성능 최적화
    간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Inflearn_JPA_Usage_01/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -100,4 +100,12 @@ public class OrderRepository {
                                 " join fetch o.delivery d", Order.class)
                 .getResultList(); // fetch join
     }
+
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery("select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
 }

c36040b5dcfed8ba24d77ebdf91c3f70a48c8c5c
DESKTOP-UNJHOPU\user <c5ngsdp@gmail.com>
2024-08-01 08:59:42 +0900

    Section 4. 쿼리 메소드 기능
    반환 타입

--- Inflearn_Spring_Data_JPA/src/test/java/c5ng/data_jpa/repository/MemberRepositoryTest.java
+++ Inflearn_Spring_Data_JPA/src/test/java/c5ng/data_jpa/repository/MemberRepositoryTest.java
@@ -145,4 +145,17 @@ class MemberRepositoryTest {
             System.out.println("member = " + member);
         }
     }
+
+    @Test
+    public void returnType() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Member aaa = memberRepository.findMemberByUsername("AAA");
+        System.out.println("aaa = " + aaa);
+        Optional<Member> optionalMember = memberRepository.findOptionalByUsername("BBB");
+        System.out.println("optionalMember = " + optionalMember.get());
+    }
 }

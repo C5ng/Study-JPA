37053006b69b5946d0f879169540068f67034851
공대표 <c5ngsdp@gmail.com>
2024-07-31 22:08:02 +0900

    Section 4. 쿼리 메소드 기능
    JPA  NamedQuery

--- Inflearn_Spring_Data_JPA/src/main/java/c5ng/data_jpa/repository/MemberRepository.java
+++ Inflearn_Spring_Data_JPA/src/main/java/c5ng/data_jpa/repository/MemberRepository.java
@@ -2,9 +2,14 @@ package c5ng.data_jpa.repository;
 
 import c5ng.data_jpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername") // NamedQuery
+    List<Member> findByUsername(@Param("username") String username);
 }

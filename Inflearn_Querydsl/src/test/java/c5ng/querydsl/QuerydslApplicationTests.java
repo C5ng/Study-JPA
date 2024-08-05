5cce693bab96b95e3a314a189db45545c56d8316
공대표 <c5ngsdp@gmail.com>
2024-08-05 21:41:15 +0900

    Section 1. 프로젝트 환경설정
    Querydsl 설정과 검증

--- Inflearn_Querydsl/src/test/java/c5ng/querydsl/QuerydslApplicationTests.java
+++ Inflearn_Querydsl/src/test/java/c5ng/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,35 @@
 package c5ng.querydsl;
 
+import c5ng.querydsl.entity.Hello;
+import c5ng.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
